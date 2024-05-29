99edf7fea85cee82ef654a783d7987c1f07b3003
zoli2600 <zoli2600@gmail.com>
2024-05-29 18:42:49 +0200

    added callback func and arrow func

--- script.js
+++ script.js
@@ -1,2 +1,60 @@
-console.log("hello world")
+/*console.log("hello world")
 
+helloWorld()
+
+function helloWorld () {
+    console.log("Hello world from the function")
+
+helperFunction()
+
+function helperFunction () {
+    console.log("i am helping")
+}
+
+    const functionVariable = "text"
+    console.log(functionVariable)
+}
+
+console.log("hello world 2")
+
+const coolerHelloWorld = function() {
+    console.log("hello world from the cooler function")
+}
+
+coolerHelloWorld()
+
+const coolestHelloWorld = () => {
+    console.log("hello world from the coolest function")
+}*/
+
+function sumOfTwoNumbers(number1, number2){
+    return number1 + number2
+}
+
+function multiplyTwoNumbers(number1, number2){
+    return (number1 * number2)
+}
+/*const result = sumOfTwoNumbers(5, 10)
+console.log("result: ", result)*/
+
+console.log(sumOfTwoNumbers(20,2))
+
+// AHELYETT, HOGY ÍGY ÍRNÁNK A NYÍL FÜGGVÉNYT:
+
+/*const arrowSumOfTwoNumbers = (number1, number2) => {
+    return number1 + number2
+}*/
+
+// íGY IS ÍRHATJUK:
+
+const arrowSumOfTwoNumbers = (number1, number2) => number1 + number2
+
+
+console.log(arrowSumOfTwoNumbers(1,2))
+
+function doSomeMagic(number1, number2, callback) {
+    return callback(number1, number2)
+}
+
+console.log(doSomeMagic(1, 2, sumOfTwoNumbers))
+console.log(doSomeMagic(1, 2, multiplyTwoNumbers))
